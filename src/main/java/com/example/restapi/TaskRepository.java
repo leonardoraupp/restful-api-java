4c0057264a2bf60b748a06c12014e1c32ac9394e
Leonardo Raupp <leonardoraupprodrigues@hotmail.com>
2024-09-15 22:54:38 -0300

    comment update

--- src/main/java/com/example/restapi/TaskRepository.java
+++ src/main/java/com/example/restapi/TaskRepository.java
@@ -2,7 +2,7 @@ package com.example.restapi;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-//A repository interface to handle database operations. This interface extends the JpaRepository.
+//A repository interface to handle database operations(Data access). This interface extends the JpaRepository.
 //The repository uses the task class(model) in the database operations.
 public interface TaskRepository extends JpaRepository<Task, Long> {
 }
