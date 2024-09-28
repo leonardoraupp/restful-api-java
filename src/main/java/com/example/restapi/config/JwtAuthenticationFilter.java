881f777705fa507b438a2027bb3507dd02fa5a4e
Leonardo Raupp <leonardoraupprodrigues@hotmail.com>
2024-09-27 22:36:57 -0300

    implement JwtAuthenticationFilter class.

---
+++ src/main/java/com/example/restapi/config/JwtAuthenticationFilter.java
@@ -0,0 +1,25 @@
+package com.example.restapi.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+
+    }
+}
+// We extends the class OncePerRequestFilter because  this class need to be fired always a request happens in the system.
+// This will be the first layer when the client or user hit a http request.
