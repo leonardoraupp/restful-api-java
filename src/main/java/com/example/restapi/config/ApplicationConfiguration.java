a25ccacf4402fc46fdc85d44951434daf734b70b
Leonardo Raupp <leonardoraupprodrigues@hotmail.com>
2024-10-04 08:56:20 -0300

    implement app config and security config

--- src/main/java/com/example/restapi/config/ApplicationConfiguration.java
+++ src/main/java/com/example/restapi/config/ApplicationConfiguration.java
@@ -3,9 +3,16 @@ package com.example.restapi.config;
 import com.example.restapi.user.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class ApplicationConfiguration {
@@ -20,8 +27,27 @@ public class ApplicationConfiguration {
         return new UserDetailsService() {
             @Override
             public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-            return userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+                return userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
             }
         };
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager(); // use the default config
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService());
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider; // this provider has the user details who will be used in the security configuration class;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(); // provide an encrypted password
+    }
+
 }
